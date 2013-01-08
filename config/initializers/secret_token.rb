8c5ff273c79e6779e9933e4b3f081c4ef2e36936
Jane Saint <papertrail@hotmail.com>
2013-01-07 21:59:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticPages::Application.config.secret_token = '5c14db1d6878d63eb6c9887529147509463f7be2e70c943d23646fec1a73b452b3a4e6308d3fd6d858c71af04021021a1006ccde0920a6d2e563c2d58b87c0e5'
