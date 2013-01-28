03c5b371b3e0c20895c20e4cbcf1284515446401
Jane Saint <papertrail@hotmail.com>
2013-01-27 20:57:26 -0800

    Adding email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 50}
+  validates(:email, { presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}})
 end
 
